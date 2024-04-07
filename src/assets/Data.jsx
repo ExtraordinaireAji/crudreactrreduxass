2e9b8553bc68afd39d58d2e404fe557a0e03f78b
Toheeb Ajiboye <Ajiboyetoheeb10@gmail.com>
2024-04-07 11:00:46 +0100

    crudreactredux

---
+++ src/assets/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "Yousef Khan",
+        email: "Yousefkhan@gmail.com",
+        id: 1
+    },
+    {
+        name: "Khalil",
+        email: "khalil@gmail.com",
+        id: 2
+    },
+    {
+        name: "Yousef Mo",
+        email: "YousefMo@gmail.com",
+        id: 3
+    }
+]
